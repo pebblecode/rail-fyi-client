b7bec4492b320b2a68cfd4e03a0864d8638418c2
Sammy Isseyegh <sammy@pebblecode.com>
2016-05-17 16:01:26 +0100

    add express server to use heroku

--- app/js/Routes.js
+++ app/js/Routes.js
@@ -1,15 +1,14 @@
 'use strict';
 
-import React                       from 'react';
+import React from 'react';
 import {Router, Route, IndexRoute} from 'react-router';
-import CreateBrowserHistory        from 'history/lib/createBrowserHistory';
-
-import App                         from './App';
-import HomePage                    from './pages/HomePage';
-import SearchPage                  from './pages/SearchPage';
-import NotFoundPage                from './pages/NotFoundPage';
-import StaffForm                   from './components/StaffForm';
-import NearestStation              from './components/NearestStation';
+import CreateBrowserHistory from 'history/lib/createBrowserHistory';
+import App from './App';
+import HomePage from './pages/HomePage';
+import SearchPage from './pages/SearchPage';
+import NotFoundPage from './pages/NotFoundPage';
+import StaffForm from './components/StaffForm';
+import NearestStation from './components/NearestStation';
 
 export default (
   <Router history={CreateBrowserHistory()}>
---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use('*', function(req, res){
+  res.sendFile(path.join(__dirname, 'build/index.html'));
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Listening on port 3000');
+});
