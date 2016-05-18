4eb3e67ca60eaeda40f8a36db1c56abac48524c9
Emma Nash <emma.nash@pebblecode.com>
2016-05-18 11:46:22 +0100

    Add station autocomplete component

--- app/js/pages/SelectStation.js
+++ app/js/pages/SelectStation.js
@@ -2,10 +2,17 @@
 
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import Autocomplete from 'react-autocomplete';
 import Progress from '../components/Progress';
 import NearestStation from '../components/NearestStation';
+import stationList from '../data/station-list';
 
 class SelectStation extends Component {
+  constructor(props) {
+    super(props);
+    this.stationList = stationList.map(station => station.name);
+  }
+
   render() {
     return (
       <section className="select-station">
@@ -14,7 +21,18 @@ class SelectStation extends Component {
         <p>Which station would you like to give us feedback on?</p>
         <NearestStation onStationClick='/select-subject' />
         <p>Not the right station? Select below:</p>
-        <textbox />
+        <Autocomplete
+          items={this.stationList}
+          getItemValue={station => station}
+          shouldItemRender={(station, value) => value !== '' && station.toLowerCase().indexOf(value.toLowerCase()) !== -1}
+          onSelect={value => console.log('Redirect to next page', value)}
+          renderItem={(station, isHighlighted) => (
+              <div
+                  style={isHighlighted ? {'backgroundColor': 'yellow'} : {}}
+                  key={station}>
+                {station}
+              </div>
+          )} />
       </section>
     );
   }
