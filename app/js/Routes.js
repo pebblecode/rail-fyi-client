57c6d7009bc200b2dcbe37418c2a84675d66709d
Sammy Isseyegh <sammy@pebblecode.com>
2016-05-18 10:51:24 +0100

    Wrap React Router with redux Provider && connect homePage to store

--- app/js/Routes.js
+++ app/js/Routes.js
@@ -1,7 +1,9 @@
 'use strict';
 
 import React from 'react';
-import {Router, Route, IndexRoute} from 'react-router';
+import { Router, Route, IndexRoute } from 'react-router';
+import { Provider } from 'react-redux';
+import ConfigureStore from './store';
 import CreateBrowserHistory from 'history/lib/createBrowserHistory';
 import App from './App';
 import HomePage from './pages/HomePage';
@@ -11,18 +13,20 @@ import NearestStation from './components/NearestStation';
 import SelectSubject from './pages/SelectSubject';
 
 export default (
-  <Router history={CreateBrowserHistory()}>
-    <Route path="/" component={App}>
+  <Provider store={ConfigureStore()}>
+    <Router history={CreateBrowserHistory()}>
+      <Route path="/" component={App}>
 
-      <IndexRoute component={HomePage} />
+        <IndexRoute component={HomePage} />
 
-      <Route path="/" component={HomePage} />
-      <Route path="/staff-form" component={StaffForm} />
-      <Route path="/nearest-station" component={NearestStation} />
-      <Route path="/select-subject" component={SelectSubject} />
+        <Route path="/" component={HomePage} />
+        <Route path="/staff-form" component={StaffForm} />
+        <Route path="/nearest-station" component={NearestStation} />
+        <Route path="/select-subject" component={SelectSubject} />
 
-      <Route path="*" component={NotFoundPage} />
+        <Route path="*" component={NotFoundPage} />
 
-    </Route>
-  </Router>
+      </Route>
+    </Router>
+  </Provider>
 );
