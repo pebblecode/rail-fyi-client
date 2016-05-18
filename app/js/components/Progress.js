89de35e86cb9edd5ffb94e36d781a3d22f7e4a4d
Emma Nash <emma.nash@pebblecode.com>
2016-05-18 09:28:48 +0100

    Add basic progress component to Select Subject

--- app/js/components/Progress.js
+++ app/js/components/Progress.js
@@ -1,19 +1,19 @@
 'use strict';
 
-import React { PropTypes, Component } from 'react';
+import React, { PropTypes, Component } from 'react';
 
 class Progress extends Component {
   render() {
     return (
       <div className="progress-bar">
-        { this.props.state }
+        { this.props.stage }
       </div>
     );
   }
 }
 
 Progress.propTypes = {
-  stage: PropTypes.int
+  stage: PropTypes.number
 };
 
 export default Progress;
--- app/js/pages/SelectSubject.js
+++ app/js/pages/SelectSubject.js
@@ -2,11 +2,13 @@
 
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import Progress from '../components/Progress';
 
 class SelectSubject extends Component {
   render() {
     return (
       <section className="select-category">
+        <Progress stage={2} />
         <h1>Subject</h1>
         <p>Please select the relevant subject you would like to give feedback on</p>
         <button>Staff</button>
