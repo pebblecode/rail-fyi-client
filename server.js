6e4ae031f5c6ec16c624bf5759f59a6fbd807e94
Sammy Isseyegh <sammy@pebblecode.com>
2016-05-18 09:29:01 +0100

    change path to build folder on server

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.use('*', function(req, res){
-  res.sendFile(path.join(__dirname, 'build/index.html'));
+  res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
 app.listen(process.env.PORT || 3000, function () {
