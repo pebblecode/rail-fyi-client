26b3ed9f9c84c68aef7e817c0e67091bb54ddd13
Emma Nash <emma.nash@pebblecode.com>
2016-05-19 16:50:27 +0100

    Update in progress message ui

--- app/js/lib/message.js
+++ app/js/lib/message.js
@@ -4,32 +4,44 @@ const buildInProgressMessage = (station, subject, description) => {
   if (!station) {
     return <div>..</div>;
   }
-  const stationLink = <Link to="/select-station">{station}</Link>;
+  const stationLink = <Link className="btn" to="/select-station">{station}</Link>;
   if (!subject) {
     return (
-      <div>
-        In {stationLink}..
+      <div className="message-container">
+        <span>At</span>
+        {stationLink}
+        <span className="ellipsis">&hellip;</span>
       </div>
     );
   }
-  const subjectLink = <Link to="/select-subject">{subject}</Link>;
+  const subjectLink = <Link className="btn" to="/select-subject">{subject}</Link>;
   if (!description) {
     return (
-      <div>
-        In {stationLink} the {subjectLink} was..
+      <div className="message-container">
+        <span>At</span>
+        {stationLink}
+        <span>the</span>
+        {subjectLink}
+        <span>was</span>
+        <span className="ellipsis">&hellip;</span>
       </div>
     );
   }
-  const descriptionLink = <Link to="/select-description">{description}</Link>;
+  const descriptionLink = <Link className="btn" to="/select-description">{description}</Link>;
   return (
-    <div>
-      In {stationLink} the {subjectLink} was {descriptionLink}
+    <div className="message-container">
+      <span>At</span>
+      {stationLink}
+      <span>the</span>
+      {subjectLink}
+      <span>was</span>
+      {descriptionLink}
     </div>
   );
 };
 
 const buildFinalMessageString = (station, subject, description) => {
-  return 'In ' + station + ' the ' + subject + ' was ' + description;
+  return 'At ' + station + ' the ' + subject + ' was ' + description;
 };
 
 export default { buildInProgressMessage, buildFinalMessageString };
