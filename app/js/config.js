8dbecc1494a7cbacd22e5242087656dc48996c93
Emma Nash <emma.nash@pebblecode.com>
2016-05-19 11:10:07 +0100

    Add station operator twitter handles

--- app/js/config.js
+++ app/js/config.js
@@ -28,4 +28,167 @@ const subjectDescriptions = {
   ]
 };
 
-export default { subjectDescriptions }
+const stationOperators = [
+  { name: 'Abellio Greater Anglia',
+    code: 'LE',
+    twitter: '@greateranglia',
+    email: ''
+  },
+  { name: 'Arriva Trains Wales',
+    code: 'AW',
+    twitter: '@ArrivaTW',
+    email: ''
+  },
+  { name: 'c2c',
+    code: 'CC',
+    twitter: '@c2c_Rail',
+    email: ''
+  },
+  { name: 'Caledonian Sleeper',
+    code: 'CS',
+    twitter: '@CalSleeper',
+    email: ''
+  },
+  { name: 'Chiltern Railway',
+    code: 'CH',
+    twitter: '@chilternrailway',
+    email: ''
+  },
+  { name: 'CrossCountry',
+    code: 'XC',
+    twitter: '@crosscountryuk',
+    email: ''
+  },
+  { name: 'East Midlands Trains',
+    code: 'EM',
+    twitter: '@EMTrains',
+    email: ''
+  },
+  { name: 'East Coast',
+    code: 'GR',
+    twitter: '@Virgin_TrainsEC',
+    email: ''
+  },
+  { name: 'Eurostar',
+    code: 'ES',
+    twitter: '@EurostarUK',
+    email: ''
+  },
+  { name: 'First Great Western',
+    code: 'GW',
+    twitter: '@GWRHelp',
+    email: ''
+  },
+  { name: 'First Hull Trains',
+    code: 'HT',
+    twitter: '@FirstHTrains',
+    email: ''
+  },
+  { name: 'First Scotrail',
+    code: 'SR',
+    twitter: '@ScotRail',
+    email: ''
+  },
+  { name: 'First Transpennine Express',
+    code: 'TP',
+    twitter: '@TPExpressTrains',
+    email: ''
+  },
+  { name: 'Gatwick Express',
+    code: 'GX',
+    twitter: '@GatwickExpress',
+    email: ''
+  },
+  { name: 'GB Railfreight',
+    code: 'ZZ',
+    twitter: '@GBRailFreight',
+    email: ''
+  },
+  { name: 'Grand Central',
+    code: 'GC',
+    twitter: '@GC_Rail',
+    email: ''
+  },
+  { name: 'Govia Thameslink Railway (Great Northern)',
+    code: 'GN',
+    twitter: '@GNRailUK',
+    email: ''
+  },
+  { name: 'Govia Thameslink Railway (Thameslink)',
+    code: 'TL',
+    twitter: '@TLRailUK',
+    email: ''
+  },
+  { name: 'Heathrow Express',
+    code: 'HX',
+    twitter: '@HeathrowExpress',
+    email: ''
+  },
+  { name: 'Island Lines',
+    code: 'IL',
+    twitter: '@NRE_Island_Line',
+    email: ''
+  },
+  { name: 'London Midland',
+    code: 'LM',
+    twitter: '@LondonMidland',
+    email: ''
+  },
+  { name: 'London Overground',
+    code: 'LO',
+    twitter: '@LDNOverground',
+    email: ''
+  },
+  { name: 'LUL Bakerloo Line',
+    code: 'LT',
+    twitter: '@bakerlooline',
+    email: ''
+  },
+  { name: 'LUL District Line – Wimbledon',
+    code: 'LT',
+    twitter: '@districtline',
+    email: ''
+  },
+  { name: 'LUL District Line – Richmond',
+    code: 'LT',
+    twitter: '@districtline',
+    email: ''
+  },
+  { name: 'Merseyrail',
+    code: 'ME',
+    twitter: '@merseyrail',
+    email: ''
+  },
+  { name: 'Northern Rail',
+    code: 'NT',
+    twitter: '@northernassist',
+    email: ''
+  },
+  { name: 'South West Trains',
+    code: 'SW',
+    twitter: '@SW_Trains',
+    email: ''
+  },
+  { name: 'Southeastern',
+    code: 'SE',
+    twitter: '@SE_Railway',
+    email: ''
+  },
+  { name: 'Southern',
+    code: 'SN',
+    twitter: '@SoutherRailUK',
+    email: ''
+  },
+  { name: 'TfL Rail (will become Crossrail)',
+    code: 'XR',
+    twitter: '@Tfl',
+    email: ''
+  },
+  { name: 'Virgin Trains',
+    code: 'VT',
+    twitter: '@VirginTrains',
+    email: ''
+  },
+];
+
+export default { subjectDescriptions, stationOperators }
--- app/js/pages/SendComplaint.js
+++ app/js/pages/SendComplaint.js
@@ -5,10 +5,17 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import ProgressBar from '../components/ProgressBar';
 import InProgressMessage from '../components/InProgressMessage';
+import stationList from '../data/station-list';
+import config from '../config';
 
 class SendComplaint extends Component {
   render() {
-    const { message } = this.props;
+    const { station, message } = this.props;
+    const stationOperatorCode =
+      stationList.filter(s => s.name === station)[0].stationOperator;
+    const stationOperator =
+      config.stationOperators.filter(o => o.code === stationOperatorCode)[0];
+    const tweet = stationOperator.twitter + ' ' + message;
     return (
       <section className="select-complaint">
         <ProgressBar stage={4} />
@@ -17,7 +24,7 @@ class SendComplaint extends Component {
         <InProgressMessage />
         Ready? Choose your method of weapon below.
         <button>Send email</button>
-        <a href={"http://twitter.com/home/?status=" + message}>
+        <a href={"http://twitter.com/home/?status=" + tweet}>
           Tweet
         </a>
       </section>
@@ -26,12 +33,15 @@ class SendComplaint extends Component {
 }
 
 SendComplaint.propTypes = {
-  message: PropTypes.string
+  message: PropTypes.string,
+  station: PropTypes.string
 };
 
 const mapStateToProps = state => {
+  // TODO: Get this from the state
   return {
-    message: "At Vauxhall the toilet is dirty" // TODO: Get this from the state
+    message: "At Vauxhall the toilet is dirty",
+    station: "Vauxhall"
   };
 };
 
