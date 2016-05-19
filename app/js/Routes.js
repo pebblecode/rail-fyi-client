a14604b453ba89a3fa8b09ca836d813dcbeb7d71
Sammy Isseyegh <sammy@pebblecode.com>
2016-05-19 09:56:44 +0100

    clean up un-used files/routes

--- app/js/Routes.js
+++ app/js/Routes.js
@@ -7,7 +7,6 @@ import App from './App';
 import HomePage from './pages/HomePage';
 import NotFoundPage from './pages/NotFoundPage';
 import StaffForm from './components/StaffForm';
-import NearestStation from './components/NearestStation';
 import SelectSubject from './pages/SelectSubject';
 import SelectStation from './pages/SelectStation';
 import SelectDescription from './pages/SelectDescription';
@@ -22,10 +21,8 @@ const Root = props => {
           <IndexRoute component={HomePage} />
 
           <Route path="/" component={HomePage} />
-          <Route path="/staff-form" component={StaffForm} />
-          <Route path="/nearest-station" component={NearestStation} />
-          <Route path="/select-subject" component={SelectSubject} />
           <Route path="/select-station" component={SelectStation} />
+          <Route path="/select-subject" component={SelectSubject} />
           <Route path="/select-description" component={SelectDescription} />
           <Route path="/send-complaint" component={SendComplaint} />
 
