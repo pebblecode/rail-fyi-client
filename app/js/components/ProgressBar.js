a6fef2158db0df648a30fcc957569a19112269c0
Emma Nash <emma.nash@pebblecode.com>
2016-05-19 08:36:49 +0100

    Put in basic progress bar visual

--- app/js/components/ProgressBar.js
+++ app/js/components/ProgressBar.js
@@ -4,9 +4,25 @@ import React, { PropTypes, Component } from 'react';
 
 class ProgressBar extends Component {
   render() {
+    const dots =
+      [1,2,3,4].map(i => {
+        return (
+          <div
+            key={i}
+            style={{
+              "width": "10px",
+              "height": "10px",
+              "border": "1px solid blue",
+              "backgroundColor": i === this.props.stage ? "blue" : "transparent",
+              "margin": "20px",
+              "display": "inline-block"
+            }}>
+          </div>
+        );
+      });
     return (
       <div className="progress-bar">
-        { this.props.stage }
+        { dots }
       </div>
     );
   }
