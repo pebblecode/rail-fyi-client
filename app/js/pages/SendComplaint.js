b6b5419d04e200d5676c86ab6c50944899ec0dbe
Emma Nash <emma.nash@pebblecode.com>
2016-05-19 08:48:53 +0100

    Add tweet functionality

--- app/js/pages/SendComplaint.js
+++ app/js/pages/SendComplaint.js
@@ -1,24 +1,38 @@
 'use strict';
 
-import React, { Component } from 'react';
+import React, { PropTypes, Component } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import ProgressBar from '../components/ProgressBar';
 import InProgressMessage from '../components/InProgressMessage';
 
 class SendComplaint extends Component {
   render() {
+    const { message } = this.props;
     return (
       <section className="select-complaint">
         <ProgressBar stage={4} />
-        Here is your message.
-        Anything else you'd like to add?
+        <div>Here is your message.</div>
+        <div>Anything else you'd like to add?</div>
         <InProgressMessage />
         Ready? Choose your method of weapon below.
         <button>Send email</button>
-        <button>Tweet</button>
+        <a href={"http://twitter.com/home/?status=" + message}>
+          Tweet
+        </a>
       </section>
     );
   }
 }
 
-export default SendComplaint;
+SendComplaint.propTypes = {
+  message: PropTypes.string
+};
+
+const mapStateToProps = state => {
+  return {
+    message: "At Vauxhall the toilet is dirty" // TODO: Get this from the state
+  };
+};
+
+export default connect(mapStateToProps)(SendComplaint);
