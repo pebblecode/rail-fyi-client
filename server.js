234091230695567a1786c0ab7d18260d8761b30c
Sammy Isseyegh <sammy@pebblecode.com>
2016-05-23 14:06:19 +0100

    return next & redirect

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ app.use(function(req, res, next) {
   var schema = req.headers['x-forwarded-proto'];
 
   if (schema === 'https') {
-    next();
+    return next();
   }
   else {
-    res.redirect('https://' + req.headers.host + req.url);
+    return res.redirect('https://' + req.headers.host + req.url);
   }
 });
 
