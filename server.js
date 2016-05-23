737a115e005cd5ea27fed5853dd4ec75f7a54cc2
Sammy Isseyegh <sammy@pebblecode.com>
2016-05-23 14:01:20 +0100

    redirect to https, always

--- server.js
+++ server.js
@@ -5,7 +5,18 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.use('*', function(req, res){
+app.use(function(req, res, next) {
+  var schema = req.headers['x-forwarded-proto'];
+
+  if (schema === 'https') {
+    next();
+  }
+  else {
+    res.redirect('https://' + req.headers.host + req.url);
+  }
+});
+
+app.use('*', function(req, res) {
   res.sendFile('./build/index.html', {root: __dirname})
   // res.sendFile(path.join(__dirname, '../build/index.html'));
 });
