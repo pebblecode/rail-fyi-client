c4b75aa4effcee0e78542dbbc634263e22576165
Emma Nash <emma.nash@pebblecode.com>
2016-05-23 14:30:30 +0100

    Try to redirect to https again

--- server.js
+++ server.js
@@ -5,15 +5,20 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.use('*', function(req, res) {
-  var schema = req.headers['x-forwarded-proto'];
-  console.log(schema);
-
-  if (schema === 'https') {
-    res.sendFile('./build/index.html', {root: __dirname});
+app.use(function(req, res, next) {
+  console.log('HEY');
+  if (req.secure) {
+    console.log('ALREADY SECURE');
+    next();
   } else {
-    res.redirect('https://' + req.headers.host + req.url);
+    var https = 'https://' + res.headers.host + req.url;
+    console.log('REDIRECTING TO HTTPS', https);
+    res.redirect(https);
   }
+});
+
+app.use('*', function(req, res) {
+  res.sendFile('./build/index.html', {root: __dirname});
 });
 
 app.listen(process.env.PORT || 3000, function () {
