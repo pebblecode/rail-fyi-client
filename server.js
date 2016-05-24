da2d8a6aaae055cc7ab0f04def40554fd6c74aac
Sammy Isseyegh <sammy@pebblecode.com>
2016-05-24 13:33:59 +0100

    add keys & use forceSSL express middleware

--- server.js
+++ server.js
@@ -1,30 +1,46 @@
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
+var forceSSL = require('express-force-ssl');
 
-var app = express();
+var ssl_options = {
+  key: fs.readFileSync('./keys/private.key'),
+  cert: fs.readFileSync('./keys/cert.crt'),
+};
 
+var server = http.createServer(app);
+var secureServer = https.createServer(ssl_options, app);
+
+
+app.set('forceSSLOptions', {
+  enable301Redirects: true,
+  trustXFPHeader: false,
+  httpsPort: 443,
+  sslRequiredMessage: 'SSL Required.'
+});
+
+app.use(forceSSL);
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', function(req, res, next) {
-  console.log('HEY');
-  var schema = req.headers['x-forwarded-proto'];
-  console.log(schema);
-
-  if (schema === 'https') {
-    console.log('ALREADY SECURE');
-  } else {
-    console.log(req.headers, req.url);
-    var https = 'https://' + req.headers.host + req.url;
-    console.log('REDIRECTING TO HTTPS', https);
-    res.redirect(https);
-  }
-});
+// app.get('*', function(req, res, next) {
+//   console.log('HEY');
+//   var schema = req.headers['x-forwarded-proto'];
+//   console.log(schema);
+//
+//   if (schema === 'https') {
+//     console.log('ALREADY SECURE');
+//   } else {
+//     console.log(req.headers, req.url);
+//     var https = 'https://' + req.headers.host + req.url;
+//     console.log('REDIRECTING TO HTTPS', https);
+//     res.redirect(https);
+//   }
+// });
 
 app.use('*', function(req, res) {
   console.log('dirname is' + __dirname);
   res.sendFile('./build/index.html', {root: __dirname});
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Listening on port 3000');
-});
+secureServer.listen(443);
+server.listen(80);
